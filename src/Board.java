fe5204787e4682c66f253038ce8d51d0ecf63613
Mateusz Milczarek <milczary.mm@gmail.com>
2023-05-23 20:01:51 +0200

    Dodana klasa Board i enum SpecialFieldTypes oraz ich implementacja

---
+++ src/Board.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Random;
+
+public class Board {
+    //mapa (bez domków) sklada sie z 56 pól, czyli 4 czesci po 14 pol
+
+    private final HashMap<Integer, SpecialFieldTypes> specialFields = new HashMap<Integer, SpecialFieldTypes>();
+    public Board() {
+        Random random = new Random();
+        SpecialFieldTypes specialField = SpecialFieldTypes.values()[random.nextInt(7)];
+        int lowerLimit = 1, upperLimit = 14;
+
+        // losowanie dla jednej z 4 czesci po trzy pola specjalne
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 3; j++) {
+                int nextRand = random.nextInt(upperLimit - lowerLimit) + lowerLimit;
+                while (specialFields.containsKey(nextRand)) {
+                    nextRand = random.nextInt(upperLimit - lowerLimit) + lowerLimit;
+                }
+                specialFields.put(nextRand, specialField);
+                specialField = specialField.next();
+            }
+            lowerLimit += 14;
+            upperLimit += 14;
+        }
+    }
+    public SpecialFieldTypes getSpecialField(int coordinate) {
+        return specialFields.get(coordinate);
+    }
+}

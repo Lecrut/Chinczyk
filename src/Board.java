9fc807d588aca36927cde0bb02390d2474fd7313
Mateusz Milczarek <milczary.mm@gmail.com>
2023-05-24 11:08:31 +0200

    Refaktoryzacja kodu

--- src/Board.java
+++ src/Board.java
@@ -3,14 +3,21 @@ import java.util.Random;
 
 public class Board {
     //mapa (bez domków) sklada sie z 56 pól, czyli 4 czesci po 14 pol
-
     private final HashMap<Integer, SpecialFieldTypes> specialFields = new HashMap<>();
+
     public Board() {
+        // losowanie dla jednej z 4 czesci po trzy pola specjalne
+        generateSpecialFields();
+    }
+
+    public SpecialFieldTypes getSpecialField(int coordinate) {
+        return specialFields.get(coordinate);
+    }
+
+    private void generateSpecialFields() {
         Random random = new Random();
         SpecialFieldTypes specialField = SpecialFieldTypes.values()[random.nextInt(7)];
         int lowerLimit = 1, upperLimit = 14;
-
-        // losowanie dla jednej z 4 czesci po trzy pola specjalne
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 3; j++) {
                 int nextRand = random.nextInt(upperLimit - lowerLimit) + lowerLimit;
@@ -23,8 +30,5 @@ public class Board {
             lowerLimit += 14;
             upperLimit += 14;
         }
-    }
-    public SpecialFieldTypes getSpecialField(int coordinate) {
-        return specialFields.get(coordinate);
     }
 }

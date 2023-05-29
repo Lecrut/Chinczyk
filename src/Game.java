571a1162dee7e0c8ee05d356e4a58ccece7f101b
Mateusz Milczarek <milczary.mm@gmail.com>
2023-05-29 18:09:40 +0200

    Rozwiazany konflikt w Game, dodany argument przy wywolaniu konstruktora Game

--- src/Game.java
+++ src/Game.java
@@ -2,11 +2,103 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Game extends JFrame {
-    private final Board board = new Board();
-    public Game() throws HeadlessException {
+    private final Player[] players;
+    private final Board board;
+
+    public Game(int playersNumber) throws HeadlessException {
+        players = new Player[playersNumber];
+        board = new Board();
         setFrameParameters();
+
         //TODO tymczasowy pionek
         board.setPawn(new Pawn(new ImageIcon("assets/pawn.png")), 0);
+
+        if (playersNumber >= 1)
+            players[0] = new Player(Colour.GREEN, 0, 56);
+        if (playersNumber >= 2)
+            players[1] = new Player(Colour.BLUE, 28, 27);
+        if (playersNumber >= 3)
+            players[2] = new Player(Colour.RED, 14, 13);
+        if (playersNumber == 4)
+            players[3] = new Player(Colour.YELLOW, 42, 41);
+    }
+
+    public void round() {
+        for (Player player : players) {
+            player.playerMove();
+            checkBoard(player);
+        }
+    }
+
+    private void checkSpecialFields(Player player) {
+        for (Pawn pawn : player.pawns) {
+            if (pawn.getStatus() == PawnStatuses.IN_GAME) {
+                SpecialFieldTypes specialFieldType = board.getSpecialField((pawn.getPosition() + player.getFirstField()) % 61);
+                triggerSpecialFields(pawn, specialFieldType);
+                for (Player player1 : players) {
+                    checkCollisionPlayer(player, player1);
+                }
+            }
+        }
+    }
+
+    private int teleportPawn() {
+        while (true) {
+            int x = (int) (Math.random() * 56);
+            if (board.getSpecialField(x) == null) {
+                return x;
+            }
+        }
+    }
+
+    private void triggerSpecialFields(Pawn pawn, SpecialFieldTypes fieldType) {
+        if (fieldType == null)
+            return;
+        switch (fieldType) {
+            case FORWARD_1 -> pawn.move(1);
+            case FORWARD_2 -> pawn.move(2);
+            case FORWARD_3 -> pawn.move(3);
+            case BACKWARD_1 -> pawn.move(-1);
+            case BACKWARD_2 -> pawn.move(-2);
+            case BACKWARD_3 -> pawn.move(-3);
+            case TELEPORT -> pawn.setPosition(teleportPawn());
+        }
+    }
+
+    private void checkCollisionPlayer(Player player1, Player player2) {
+        if (player1.equals(player2)) {
+            return;
+        }
+        for (Pawn pawn2 : player2.pawns) {
+            if (pawn2.getStatus() == PawnStatuses.IN_GAME) {
+                for (Pawn pawn1 : player1.pawns) {
+                    if (pawn1.getStatus() == PawnStatuses.IN_GAME) {
+                        if ((pawn1.getPosition() + player1.getFirstField()) % 61 == (pawn2.getPosition() + player2.getFirstField()) % 61) {
+                            pawn2.setStatusGame(PawnStatuses.IN_BASE);
+                            pawn2.setPosition(0);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void checkWinningPawns() {
+        for (Player player : players) {
+            for (Pawn pawn : player.pawns) {
+                if (pawn.getPosition() == 61) {
+                    pawn.setStatusGame(PawnStatuses.IN_END);
+                }
+            }
+        }
+    }
+
+    public void checkBoard(Player player) {
+        for (Player player1 : players) {
+            checkCollisionPlayer(player, player1);
+        }
+        checkWinningPawns();
+        checkSpecialFields(player);
     }
 
     private void setFrameParameters() {
